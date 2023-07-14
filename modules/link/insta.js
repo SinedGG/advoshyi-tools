477d49ce9dc6d5861c889f9d3ddda3d0b66f2ce5
Denis <51417134+SinedGG@users.noreply.github.com>
2023-07-14 20:41:44 +0300

    🍂 | Fixed insta download

--- modules/link/insta.js
+++ modules/link/insta.js
@@ -6,16 +6,18 @@ module.exports = (url) => {
   return new Promise(async (resolve, reject) => {
     try {
       const form = new FormData();
-      form.append("q", url);
+      form.append("url", url);
 
       var response = await axios.post(
-        `https://v3.saveinsta.app/api/ajaxSearch`,
+        `https://snapsave.app/uk/action.php`,
         form
       );
 
-      var html = response.data.data;
-      var str = cheerio.load(html);
-      str = str("a.abutton.btn-premium").attr("href");
+      var html = response.data.replace(`eval`, ` module.exports = `);
+      var res = _eval(html);
+      res = res.replace(/\\/g, " ");
+      var str = cheerio.load(res);
+      str = str(".download-items__btn a").attr("href");
 
       resolve(str);
     } catch (error) {
