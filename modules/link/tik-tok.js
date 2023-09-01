7e6a54d3192359eced15016206922d8d150315c9
Denis <51417134+SinedGG@users.noreply.github.com>
2023-09-01 22:24:23 +0300

    🍂 | Fixed tik-tok download

--- modules/link/tik-tok.js
+++ modules/link/tik-tok.js
@@ -1,24 +1,16 @@
 const axios = require("axios");
-const cheerio = require("cheerio");
-const _eval = require("eval");
 
 module.exports = (url) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const form = new FormData();
-      form.append("url", url);
+      var response = await axios.post(
+        `https://downloader.bot/api/tiktok/info`,
+        {
+          url: "https://vm.tiktok.com/ZMjdbNseQ/",
+        }
+      );
 
-      var response = await axios.post(`https://snaptik.app/abc2.php`, form);
-
-      var html = response.data.replace(`eval`, ` module.exports = `);
-      var res = _eval(html);
-      res = res.replace(/\\/g, " ");
-      var str = cheerio.load(res);
-      str = str(`.video-links`)[0].children[0].attribs.href;
-
-      if (!str.includes(`snaptik.app/`)) str = `https://snaptik.app` + str;
-
-      resolve(str);
+      resolve(response.data.data.mp4);
     } catch (error) {
       reject(error);
     }
